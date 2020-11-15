4688af24206c3587f304381a16687a215e6773b1
dlazaro09 <dlazaro09@gmail.com>
2020-11-15 17:08:40 -0600

    Add project files.

---
+++ WebAppFacturacion/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppFacturacion.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
