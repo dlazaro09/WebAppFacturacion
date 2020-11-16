febc8ffc1ef5f5cdd5de8cb071d8c32ebe6fc3c5
dlazaro09 <dlazaro09@gmail.com>
2020-11-15 19:10:42 -0600

    Realizando testeo unitario

---
+++ WebAppFacturacion/Facturacion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppFacturacion
+{
+    public class Facturacion
+    {
+        String nombre;
+        String metodoDePago;
+        Double total;
+
+        public void setNombre(String nombre) {
+
+            this.nombre = nombre;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public void setMetodoDePago(String metodoPago) {
+
+            this.metodoDePago = metodoPago;
+        }
+
+        public String getMetdoDepago() { return metodoDePago; }
+
+        public void setTotal(Double total) { this.total = total; }
+
+        public Double getTotal() { return total; }
+
+    }
+}
---
+++ WebAppFacturacion/FacturacionTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppFacturacion;
+namespace WebAppFacturacion
+{
+    [TestClass]
+    public class FacturacionTest
+    {
+        public void testSetNombre(String nombre) { var comprobarNombre = new Facturacion(); String result = comprobarNombre.getNombre(); Assert.Fail(result, "Douglas"); }
+        
+    }
+}
